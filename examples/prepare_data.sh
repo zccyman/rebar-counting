478fd0817aab7f2dda6a33df19c3a03f8af5b8c9
zccyman <zccyman@163.com>
2019-01-16 07:43:07 +0800

    update prepare_data.sh

--- examples/prepare_data.sh
+++ examples/prepare_data.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-rm -rf data/VOCdevkit2007/VOC2007
+TARGET_DIR=data/VOCdevkit2007/VOC2007
 
-mkdir -p data/VOCdevkit2007/VOC2007
+rm -rf $TARGET_DIR
 
-cp -rf data/VOCdevkit2007/VOC2007_origin/* data/VOCdevkit2007/VOC2007
-cp -rf data/VOCdevkit2007/VOC2007_rotation/* data/VOCdevkit2007/VOC2007
-cp -rf data/VOCdevkit2007/VOC2007_flip/* data/VOCdevkit2007/VOC2007
+mkdir -p $TARGET_DIR
+
+cp -rf data/VOCdevkit2007/VOC2007_origin/* $TARGET_DIR
+
+rotation_array=(-180 -90 -6 -4 -2 0 2 4 6 90) 
+for ((k=0;k<${rotation_array[@]};k++))
+do 
+cp -rf data/VOCdevkit2007/VOC2007_rotation_${rotation_array[k]}/* $TARGET_DIR
+done
+
+flip_array=(-1 0 1) 
+for ((k=0;k<${flip_array[@]};k++))
+do 
+cp -rf data/VOCdevkit2007/VOC2007_rotation_${flip_array[k]}/* $TARGET_DIR
+done
 
 rm -rf data/VOCdevkit2007/VOC2007/ImageSets/Main/*.txt
 
