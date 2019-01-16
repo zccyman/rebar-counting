637ab1016d7ced1e0f59266f77324c3313ccb07a
zccyman <zccyman@163.com>
2019-01-16 21:41:57 +0800

    update prepare_data.sh

--- examples/prepare_data.sh
+++ examples/prepare_data.sh
@@ -8,22 +8,22 @@ mkdir -p $TARGET_DIR
 
 cp -rf data/VOCdevkit2007/VOC2007_origin/* $TARGET_DIR
 
-rotation_array=(-180 -90 -6 -4 -2 0 2 4 6 90) 
-for ((k=0;k<${rotation_array[@]};k++))
+rotation_array=(-180 -90 -6 -4 -2 0 2 4 6 90 180) 
+for k in ${rotation_array[@]}
 do 
-cp -rf data/VOCdevkit2007/VOC2007_rotation_${rotation_array[k]}/* $TARGET_DIR
+cp -rf data/VOCdevkit2007/VOC2007_rotation_${k}/* $TARGET_DIR
 done
 
 flip_array=(-1 0 1) 
-for ((k=0;k<${flip_array[@]};k++))
+for k in ${flip_array[@]}
 do 
-cp -rf data/VOCdevkit2007/VOC2007_rotation_${flip_array[k]}/* $TARGET_DIR
+cp -rf data/VOCdevkit2007/VOC2007_flip_${k}/* $TARGET_DIR
 done
 
-crop_array=(0) 
-for ((k=0;k<${crop_array[@]};k++))
+crop_array=(0 1 2) 
+for k in ${crop_array[@]}
 do 
-cp -rf data/VOCdevkit2007/VOC2007_crop_${crop_array[k]}/* $TARGET_DIR
+cp -rf data/VOCdevkit2007/VOC2007_crop_${k}/* $TARGET_DIR
 done
 
 rm -rf data/VOCdevkit2007/VOC2007/ImageSets/Main/*.txt
